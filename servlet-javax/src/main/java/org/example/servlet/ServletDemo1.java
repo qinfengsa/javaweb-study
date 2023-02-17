208da39267b9b39003bff737aa0a8c00342ce69e
wangheng <15800012123@163.com>
2023-02-17 18:58:48 +0800

    servlet 请求参数

--- servlet-javax/src/main/java/org/example/servlet/ServletDemo1.java
+++ servlet-javax/src/main/java/org/example/servlet/ServletDemo1.java
@@ -1,6 +1,8 @@
 package org.example.servlet;
 
 import org.example.util.ResponseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -15,17 +17,29 @@ import java.io.IOException;
  */
 public class ServletDemo1 extends HttpServlet {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServletDemo1.class);
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String         uri     = request.getRequestURI();
 		ServletContext context = getServletContext();
 		context.setAttribute("uri", uri);
+
+		String name = request.getParameter("name");
+
+		String[] names = request.getParameterValues("name");
+		LOGGER.info("name:{}", name);
+		LOGGER.info("names:{}", String.join(",", names));
+
 		ResponseUtils.printHtml(response, "A Servlet Demo", " This is " + this.getClass() + ", using the GET method");
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		String   name  = request.getParameter("name");
+		String[] names = request.getParameterValues("name");
+		LOGGER.info("name:{}", name);
+		LOGGER.info("names:{}", String.join(",", names));
 		ResponseUtils.printHtml(response, "A Servlet Demo", " This is "
 				+ this.getClass() + ", using the POST method");
 
